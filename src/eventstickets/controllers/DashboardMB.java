bcd53816700fee94dd7e4be96be5b1e4c538ba4f
Thiago Guimaraes <thiagogsr@gmail.com>
2015-11-22 18:20:37 -0300

    Don't show dashboard for non registered users

--- src/eventstickets/controllers/DashboardMB.java
+++ src/eventstickets/controllers/DashboardMB.java
@@ -2,31 +2,34 @@ package eventstickets.controllers;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Map;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import javax.faces.context.FacesContext;
 
 import eventstickets.dao.EventInscriptionDAO;
 import eventstickets.helpers.MessageHelper;
 import eventstickets.models.Event;
+import eventstickets.policies.EventPolicy;
 
 @ManagedBean(name = "dashboardMB")
 @RequestScoped
 public class DashboardMB extends AuthenticateUser implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
+
 	public DashboardMB() {
 		new AuthenticateUser();
 		if (params().get("denied") != null) {
 			MessageHelper.addMensage("Você não tem permissão para acessar esta página.", FacesMessage.SEVERITY_ERROR);
 		}
 	}
-	
+
 	public List<Event> getEvents() {
 		EventInscriptionDAO dao = new EventInscriptionDAO();
 		return dao.getRegisteredEvents(getCurrentUser().getId());
+	}
+
+	public Boolean getAllowed() {
+		return EventPolicy.register(getCurrentUser());
 	}
 }
