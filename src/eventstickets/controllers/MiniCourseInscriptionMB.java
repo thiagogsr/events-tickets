b23f269307dbc7868df7a24db6ca78f330aa8393
Thiago Guimaraes <thiagogsr@gmail.com>
2015-11-23 21:11:33 -0300

    Fixed eventID param on mini course inscription

--- src/eventstickets/controllers/MiniCourseInscriptionMB.java
+++ src/eventstickets/controllers/MiniCourseInscriptionMB.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
 import eventstickets.dao.EventDAO;
@@ -23,6 +24,8 @@ public class MiniCourseInscriptionMB  extends AuthenticateUser implements Serial
 	private static final long serialVersionUID = 1L;
 	private MiniCourseInscription miniCourseInscription = new MiniCourseInscription();
 	private Integer miniCourseId;
+
+	@ManagedProperty(value="#{param.eventId}")
 	private String eventId;
 
 	public MiniCourseInscriptionMB() {
@@ -31,12 +34,10 @@ public class MiniCourseInscriptionMB  extends AuthenticateUser implements Serial
 	}
 
 	public List<MiniCourse> getNotRegistered() {
-		trySetEventId();
 		return new MiniCourseDAO().notRegistered(getCurrentUser().getId(), Integer.parseInt(getEventId()));
 	}
 
 	public List<MiniCourseInscription> getRegistered() {
-		trySetEventId();
 		return new MiniCourseInscriptionDAO().registered(Integer.parseInt(getEventId()));
 	}
 
@@ -51,7 +52,7 @@ public class MiniCourseInscriptionMB  extends AuthenticateUser implements Serial
 		} else {
 			MessageHelper.addMensage("Não há mais vagas para o Mini Curso " + miniCourse.getTitle() + ".", FacesMessage.SEVERITY_ERROR);
 		}
-		return "index?eventId=#{eventId}";
+		return "index?faces-redirect=true&includeViewParams=true";
 	}
 
 	private MiniCourse getMiniCourse(){
@@ -83,14 +84,7 @@ public class MiniCourseInscriptionMB  extends AuthenticateUser implements Serial
 	}
 
 	public Event getEvent() {
-		trySetEventId();
 		return new EventDAO().find(Integer.parseInt(getEventId()));
-	}
-
-	private void trySetEventId() {
-		if (eventId == null) {
-			this.setEventId(params().get("eventId"));
-		}
 	}
 }
 
